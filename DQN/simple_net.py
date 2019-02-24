e93b1baac03cbe75dc71cecd3bf838479ed1ca6a
liancaijiang <liancaijiang@cambricon.com>
2019-02-24 19:21:48 +0800

    feat: add DQN BlackJackAI version 0

--- DQN/simple_net.py
+++ DQN/simple_net.py
@@ -5,13 +5,18 @@ from generate_dataset import *
 
 # as first layer in a sequential model:
 model = Sequential()
-model.add(Dense(10, input_shape=(3,)))
+model.add(Dense(16, input_shape=(3,)))
 # now the model will take as input arrays of shape (*, 16)
 # and output arrays of shape (*, 32)
 
 # after the first layer, you don't need to specify
 # the size of the input anymore:
-model.add(Dense(10))
+model.add(Activation('relu'))
+model.add(Dense(32))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(8))
 model.add(Activation('relu'))
 model.add(Dense(2))
 model.add(Activation('softmax'))
@@ -25,23 +30,35 @@ import numpy as np
 # data = np.random.random((1000, 100))
 # labels = np.random.randint(2, size=(1000, 1))
 
-data = []
-labels = []
 
+data_set = []
 for i in range(len(samples)):
     if samples[i].hit_num + samples[i].stand_num < 100:
         continue
     a, b, c = samples[i].state
-    data.append([a, b, c])
+    data_set.append([a, b, c])
     t = [0, 0]
     t[samples[i].action] = 1
-    labels.append(t)
+    data_set[-1] += t
+
+real_len = len(data_set)
+data_set = np.array(data_set)
+data_set_tmp0 = np.zeros(data_set.shape)
+data_set_tmp0[:][:] = data_set[:][:]
+np.random.shuffle(data_set_tmp0)
+
+data_set = np.concatenate((data_set, data_set_tmp0), axis = 0)
+
+data = data_set[:, 0:3]
+labels = data_set[:, 3:5]
 
 print('Training Data Size:', len(data))
 
 print(labels)
 
-model.fit(np.array(data), np.array(labels), epochs=10, batch_size=16)
+model.fit(data, labels, epochs=200, batch_size=16)
+
+#model.fit(data, labels, epochs=50, batch_size=16)
 
 test_data = np.array(data)
 print('ndim = ', test_data[0:3].ndim, test_data[0:3].shape)
@@ -55,14 +72,14 @@ def act(a):
         return 1
 model.predict(test_data[0:1])
 num_right = 0
-for i,s in enumerate(data):
+for i,s in enumerate(data[0:real_len]):
     ss = np.array([s])
     a = model.predict(ss)
-    print(s, a[0], act(a[0]))
+    # print(s, a[0], act(a[0]))
     if act(a[0]) == act(labels[i]):
         num_right += 1
 
-print(num_right, '/', len(data))
+print(num_right, '/', real_len)
 
-model.save('BlackJack_keras.h5')
+model.save('BlackJack_keras_v0.h5')
 
--- ai_dqn.py
+++ ai_dqn.py
@@ -4,14 +4,25 @@ import numpy as np
 
 
 class BlackJackAI():
-    def __init__(self, model_name = './DQN/BlackJack_keras.h5'):
+    def __init__(self, model_name = './DQN/BlackJack_keras_v0.h5'):
         self.name = 'AI_DQN'
         self.model = load_model(model_name)
+        self._GetPolicy()
+
+    def _GetPolicy(self):
+        self.policy = {}
+        for i in range(1, 22):
+            for j in range(1, 12):
+                _state = np.array([[i, j, 0]])
+                act = self.model.predict(_state)
+                self.policy[(i,j,0)] = np.argmax(act)
+                _state = np.array([[i, j, 1]])
+                act = self.model.predict(_state)
+                self.policy[(i,j,1)] = np.argmax(act)
+
 
     def action(self, state):
-        _state = np.array([[state[0], state[1], state[2]]])
-        act = self.model.predict(_state)
-        return np.argmax(act)
+        return self.policy[(state[0], state[1], state[2])]
 
 if __name__ == '__main__':
     ai = BlackJackAI()
